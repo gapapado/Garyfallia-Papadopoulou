2b7b6f8ecc154e25f3395614c64c5b8e71eeffde
gapapado <56559645+gapapado@users.noreply.github.com>
2020-09-22 19:21:01 +0300

    Add files via upload

---
+++ www/server-dccp.cc
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+
+#define SERVER_PORT 3007
+#define MAX_CLIENTS 1024
+
+#define SOL_DCCP 269
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	cout << "Content-type:text/plain\n\n" << endl;
+
+	char* bytes = strtok(getenv("QUERY_STRING"), "=");
+	char* bytes1 = strtok(NULL, "=");
+	int bytes_for_transfer = atoi(bytes1);
+
+	int master_socket, new_socket, client_socket[MAX_CLIENTS], client_reads[MAX_CLIENTS], max_clients = MAX_CLIENTS, i, bytes_read = 0, total_bytes_read = 0, sd;
+	int max_sd;
+	struct sockaddr_in address;
+	fd_set readfds;
+	char buffer[1025];
+	memset(client_socket, 0, sizeof(client_socket));
+	memset(client_reads, 0, sizeof(client_reads));
+	memset(&address, 0, sizeof(address));
+
+	// Create DCCP master socket
+	if ((master_socket = socket(PF_INET, SOCK_DCCP, IPPROTO_DCCP)) == 0) {
+		//perror("DCCP: Socket failed\n");
+		cout << "DCCP: Socket failed\n";
+		exit(EXIT_FAILURE);
+	}
+	// Setup address
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons( SERVER_PORT);
+
+	// Bind and listen to master socket, allow 5 pending connections
+	if (bind(master_socket, (struct sockaddr *) &address, sizeof(address)) < 0) {
+		cout << "DCCP: Bind failed" << endl;
+		exit(EXIT_FAILURE);
+	}
+	if (listen(master_socket, 5) < 0) {
+		//perror("DCCP: Listen failed\n");
+		cout << "DCCP: Listen failed" << endl;
+		exit(EXIT_FAILURE);
+	}
+	//printf("DCCP: Listening on port %d\n", SERVER_PORT);
+	cout << "DCCP: Listening on port " << SERVER_PORT << endl;
+	while (1) {
+		// Rebuild socket descriptor set
+		FD_ZERO(&readfds);
+		FD_SET(master_socket, &readfds);
+		max_sd = master_socket;
+		// Add all valid sockets to the set, find max socket number
+		for (i = 0; i < max_clients; i++) {
+			sd = client_socket[i];
+			if (sd > 0)
+				FD_SET(sd, &readfds);
+			if (sd > max_sd)
+				max_sd = sd;
+		}
+		// Block until socket activity detected
+		if ((select(max_sd + 1, &readfds, NULL, NULL, NULL) < 0)) {
+			//perror("DCCP: Select failed\n");
+			cout << "DCCP: Select failed\n";
+			exit(EXIT_FAILURE);
+		}
+		// If master socket was selected, we have an incoming connection
+		if (FD_ISSET(master_socket, &readfds)) {
+			if ((new_socket = accept(master_socket, 0, (socklen_t *) &address)) < 0) {
+				//perror("DCCP: Accept failed\n");
+				cout << "DCCP: Accept failed\n";
+				exit(EXIT_FAILURE);
+			}
+			//printf("DCCP: New connection: %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+			cout << "DCCP: New connection: " << inet_ntoa(address.sin_addr) << ":" << ntohs(address.sin_port) << endl;
+			// Add new socket descriptor
+			for (i = 0; i < max_clients; i++) {
+				if (client_socket[i] == 0) {
+					client_socket[i] = new_socket;
+					break;
+				}
+			}
+		}
+		// Check other sockets for activity
+		for (i = 0; i < max_clients; i++) {
+			sd = client_socket[i];
+			if (FD_ISSET(sd, &readfds)) {
+				getpeername(sd, (struct sockaddr*) &address, (socklen_t*) &address);
+				// Check if it was for closing , and also read the incoming message
+				bytes_read = recv(sd, buffer, sizeof(buffer), 0);
+				total_bytes_read += bytes_read;
+				if (bytes_read == 0) {
+					printf("DCCP: Disconnected: %s:%d total %d bytes\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port), client_reads[i]);
+					//cout << "DCCP: Disconnected: " << inet_ntoa(address.sin_addr) << ":" << ntohs(address.sin_port) << " total " << client_reads[i] << "bytes\n";
+					close(sd);
+					client_socket[i] = 0;
+					client_reads[i] = 0;
+				} else {
+					printf("DCCP: Read %d bytes from %s:%d\n", bytes_read, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+					//cout << "DCCP: Read " << bytes_read << " bytes from " << inet_ntoa(address.sin_addr) << ":" << ntohs(address.sin_port) << endl;
+					client_reads[i] += bytes_read;
+				}
+			}
+		}
+		// if transfer is complete, then exit
+		if(total_bytes_read >= bytes_for_transfer)
+			break;
+	}
+	return 0;
+}
